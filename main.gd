3c61a33e1dea048b106bed648bb3738d88daf9b8
MichaelKema <mkkema02@gmail.com>
2025-09-27 18:40:56 -0400

    more updates

--- main.gd
+++ main.gd
@@ -1,1 +1,10 @@
 extends Node2D
+@onready var path: Path2D = $Path2D
+@onready var road: Line2D = $Road
+
+func _ready():
+	road.points = path.curve.get_baked_points()
+	road.width = 28                       # thickness of the road
+	road.default_color = Color("#30313a") # spooky asphalt
+	road.joint_mode = Line2D.LINE_JOINT_ROUND
+	road.end_cap_mode = Line2D.LINE_CAP_ROUND
