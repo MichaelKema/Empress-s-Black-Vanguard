90c773f6aad64037e3a4be794ddaea9402b2a7a6
MichaelKema <mkkema02@gmail.com>
2025-09-28 13:18:45 -0400

    Map plus basic tower

--- Mobs/zombie.gd
+++ Mobs/zombie.gd
@@ -2,10 +2,9 @@ extends CharacterBody2D
 
 @export var speed=100
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	get_parent().set_progress(get_parent().get_progress() + speed*delta)
